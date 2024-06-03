2be537a3834d4d349a5841bf7dea8a9113927de6
lucieDLE <dolelucie0@gmail.com>
2024-06-03 16:37:40 -0400

    add pytorch video network

--- src/py/nets/classification.py
+++ src/py/nets/classification.py
@@ -15,7 +15,8 @@ import torch.optim as optim
 import pytorch_lightning as pl
 import torch.nn.init as init
 
-from sklearn.metrics import classification_report
+import pytorchvideo.models.resnet
+from pytorchvideo.models.head import ResNetBasicHead
 
 
 class NLBlock(nn.Module):
@@ -205,7 +206,6 @@ class HystNet(pl.LightningModule):
         self.accuracy(x, y)
         self.log("val_acc", self.accuracy)
 
-
 class ResNetLSTM(pl.LightningModule):
     def __init__(self, args=None, out_features=4, class_weights=None, features=False):
         super(ResNetLSTM, self).__init__()
@@ -218,10 +218,7 @@ class ResNetLSTM(pl.LightningModule):
         if(class_weights is not None):
             class_weights = torch.tensor(class_weights).to(torch.float32)
 
-        if out_features==2:
-            self.accuracy = torchmetrics.Accuracy(task='binary', num_classes=out_features)
-        else:
-            self.accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=out_features)
+        self.accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=out_features)
 
         self.loss = nn.CrossEntropyLoss(reduction='sum', weight=class_weights)
 
@@ -418,128 +415,74 @@ class ResNetLSTM_p2(pl.LightningModule):
 
         return loss
 
-
-class ResNetLSTM3(pl.LightningModule):
-
-    def __init__(self, args=None, out_features=4, class_weights=None, features=False):
-        super(ResNetLSTM3, self).__init__()
+class pytorch3DResnet(pl.LightningModule):
+    def __init__(self, args= None, out_features=4, class_weights=None, features=False):
+        super(pytorch3DResnet, self).__init__()
 
         self.save_hyperparameters()
         self.args = args
-
-        if self.args.lfb_model:
-            self.memory_model = ResNetLSTM3.load_from_checkpoint(self.args.lfb_model, strict=True)
-            self.memory_model.eval()
-            self.memory_model.cuda()
-
-            self.length_vid = 10
 
         self.class_weights = class_weights
         self.features = features
 
         if(class_weights is not None):
             class_weights = torch.tensor(class_weights).to(torch.float32)
-            
-        self.loss = nn.CrossEntropyLoss(reduction='sum', weight=class_weights)
+
         self.accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=out_features)
+        self.loss = nn.CrossEntropyLoss(reduction='sum', weight=class_weights)
 
-        backbone = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-        layers = list(backbone.children())[:-1]
-        self.model = nn.Sequential(*layers)
-        self.lstm = nn.LSTM(2048, 512, batch_first=True)
 
-        self.fc_c = nn.Linear(512, out_features)
-        self.fc_h_c = nn.Linear(1024, 512)
-        self.nl_block = NLBlock()
-        self.dropout = nn.Dropout(p=0.2)  ## 0.2 for memory bank
-        self.time_conv = TimeConv()
+        self.backbone = torch.hub.load("facebookresearch/pytorchvideo", model="slowfast_r50", pretrained=True)
+        layers = list(self.backbone.blocks)[:-1]
 
-        init.xavier_normal_(self.lstm.all_weights[0][0])
-        init.xavier_normal_(self.lstm.all_weights[0][1])
-        init.xavier_uniform_(self.fc_c.weight)
-        init.xavier_uniform_(self.fc_h_c.weight)
+        # replace last layer with same layer but with out_feature as output size
+        resnet_head = ResNetBasicHead(dropout=nn.Dropout(p=0.5),
+                                      proj=nn.Linear(in_features=2304, out_features=out_features),
+                                      output_pool=torch.nn.AdaptiveAvgPool3d(output_size=1))
 
-    def configure_optimizers(self):
-        
-        optimizer = optim.SGD([
-                {'params': self.model.parameters()},
-                {'params': self.lstm.parameters()},
-                {'params': self.time_conv.parameters(), 'lr': self.args.lr},
-                {'params': self.nl_block.parameters(), 'lr': self.args.lr},
-                {'params': self.fc_h_c.parameters(), 'lr': self.args.lr},
-                {'params': self.fc_c.parameters(), 'lr': self.args.lr},
-                ], lr=self.args.lr / 10, momentum=0.9, dampening=0, weight_decay=5e-4, nesterov=False)
+        self.model_dict = nn.ModuleDict({
+            'slowfast':nn.Sequential(*layers),
+            'resnet_head': resnet_head,
+            })
 
-        return optimizer
-    
-    def forward(self, x, long_feature=torch.empty((0), dtype=torch.float32)):
-        ## forward for the network training
-        x = x.contiguous()
-
-        batch_size, num_frames, channels, height, width = x.size()
-        x = x.view(batch_size * num_frames, channels, height, width)
-    
-        x = self.model(x)
-        x = x.view(-1, num_frames, 2048)
-        self.lstm.flatten_parameters()
-        y, _ = self.lstm(x)
-        y = y.contiguous().view(-1, 512)
-        y = self.dropout(y)
-
-        if torch.sum(long_feature) !=0:
-            y = y[num_frames - 1::num_frames]
-
-            Lt = self.time_conv(long_feature)
-
-            y_1 = self.nl_block(y, Lt)
-            y = torch.cat([y, y_1], dim=1)
-            y = self.dropout(self.fc_h_c(y))
-            y = F.relu(y)
-            y = self.fc_c(y)
+    def forward(self, x):
+        # pdb.set_trace()
+        y = self.model_dict['slowfast'](x)
+        y = self.model_dict['resnet_head'](y)
 
         return y
 
-    def training_step(self, train_batch, batch_idx):
+    def training_step(self, batch, batch_idx):
+        # print(batch.type())
+        x,y = batch
 
-        x, y = train_batch
-        if self.args.lfb_model:
-            long_features = self.memory_model(x)
-            long_features = long_features.view(-1, self.args.num_frames, 512)
+        y_hat = self.forward(x)
 
-            x = x[:,-self.length_vid:,:,:,:]
+        loss = self.loss(y_hat, y)
+        self.log("train_loss", loss.item())
 
-            x = self.forward(x, long_features)
-        else:
 
-            x = self.forward(x)
-            x = self.fc_c(x)
-            x = x[self.args.num_frames - 1::self.args.num_frames]
-        
-        loss = self.loss(x, y)
-        self.log('train_loss', loss)
-
-        self.accuracy(x, y)
+        y_hat = torch.argmax(y_hat, dim=1)
+        self.accuracy(y_hat, y)
         self.log("train_acc", self.accuracy)
         return loss
 
-    def validation_step(self, val_batch, batch_idx):
-        x, y = val_batch
-        if self.args.lfb_model:
-            long_features = self.memory_model(x)
-            long_features = long_features.view(-1, self.args.num_frames, 512)
+    def validation_step(self, batch, batch_idx):
+        # print(batch.type())
+        x,y = batch
 
-            x = x[:,-self.length_vid:,:,:,:]
+        y_hat = self.forward(x)
 
-            x = self.forward(x, long_features)
-        else:
+        loss = self.loss(y_hat, y)
+        self.log("val_loss", loss.item())
 
-            x = self.forward(x)
-            x = self.fc_c(x)
-            x = x[self.args.num_frames - 1::self.args.num_frames]
-
-        loss = self.loss(x, y)
-        self.log('val_loss', loss, sync_dist=True)
-
-        self.accuracy(x, y)
+        y_hat = torch.argmax(y_hat, dim=1)
+        self.accuracy(y_hat, y)
         self.log("val_acc", self.accuracy)
-        return loss
+
+    def configure_optimizers(self):
+        """
+        Setup the Adam optimizer. Note, that this function also can return a lr scheduler, which is
+        usually useful for training video models.
+        """
+        return torch.optim.Adam(self.parameters(), lr=1e-1)
