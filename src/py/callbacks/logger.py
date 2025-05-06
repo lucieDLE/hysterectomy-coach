698f94c1a0c4777df4573768d03b4665091ba346
lucieDLE <dolelucie0@gmail.com>
2025-05-06 15:03:00 -0400

    WIP: ImageFormerLogger (no good mask display)

--- src/py/callbacks/logger.py
+++ src/py/callbacks/logger.py
@@ -3,6 +3,7 @@ import torchvision
 import torch
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import numpy as np
 
 class ImageLogger(Callback):
     def __init__(self, num_images=50, log_steps=10):
@@ -152,3 +153,122 @@ class ImageSegLogger(Callback):
                 plt.tight_layout()
                 trainer.logger.experiment["fig/val/predictions_boxes"].upload(fig)
                 plt.close()
+
+class ImageFormerLogger(Callback):
+    def __init__(self,max_num_image=8, log_steps=100):
+        self.log_steps = log_steps
+        self.max_num_image = max_num_image
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, unused=0):
+        if batch_idx % self.log_steps == 0:
+    
+            batch = batch
+
+            n_images = min(self.max_num_image, batch['pixel_values'].shape[0])
+
+            all_images = batch['pixel_values'][:n_images]
+            all_labels = batch['class_labels'][:n_images]
+            all_masks = batch['mask_labels'][:n_images]
+
+            all_images = [(255*img).to(torch.uint8) for img in all_images]
+
+            n_cols= int(n_images /2)
+            fig, axs = plt.subplots(2, n_cols)
+            axs = axs.flatten()
+
+            for ax, image, masks, labels in zip(axs, all_images, all_masks,all_labels):
+                masks = masks.cpu().detach().numpy()
+                labels = labels.cpu().detach().numpy()
+                multimask = np.zeros_like(masks[0])
+
+                for indice, mask in zip(labels, masks):
+                    multimask[mask==1] = indice
+                ax.imshow(image.permute(1,2,0).cpu().detach().numpy())
+                ax.imshow(multimask,alpha=0.7*(multimask>0))
+                ax.axis('off')
+
+            plt.tight_layout()
+            trainer.logger.experiment["fig/val/input"].upload(fig)
+            plt.close()
+
+            with torch.no_grad():
+                _, x_hat = pl_module(batch)
+                original_sizes = [(img.shape[1],img.shape[2]) for img in all_images]  # example sizes
+
+                results = pl_module.processor.post_process_instance_segmentation(x_hat, target_sizes=original_sizes)
+
+            fig, axs = plt.subplots(2, n_cols)
+            axs = axs.flatten()
+
+            for ax, image, result in zip(axs, all_images, results):
+                multimask = result['segmentation'].cpu().detach().numpy()
+
+                ax.imshow(image.permute(1,2,0).cpu().detach().numpy())
+                ax.imshow(multimask,alpha=0.7*(multimask>0))
+                if len(result['segments_info']) !=0 :
+                    lenth = len(result['segments_info'])
+                    print(f'non zero segments: {lenth}')
+
+                ax.axis('off')  # Optional: Turn off axes for better visualization
+
+            plt.tight_layout()
+            trainer.logger.experiment["fig/val/output"].upload(fig)
+            plt.close()
+
+
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, unused=0):
+        
+        if batch_idx % self.log_steps == 0:
+        
+            batch = batch
+
+            n_images = min(self.max_num_image, batch['pixel_values'].shape[0])
+
+            all_images = batch['pixel_values'][:n_images]
+            all_labels = batch['class_labels'][:n_images]
+            all_masks = batch['mask_labels'][:n_images]
+
+            all_images = [(255*img).to(torch.uint8) for img in all_images]
+
+            n_cols= int(n_images /2)
+            fig, axs = plt.subplots(2, n_cols)
+            axs = axs.flatten()
+
+            for ax, image, masks, labels in zip(axs, all_images, all_masks,all_labels):
+                masks = masks.cpu().detach().numpy()
+                labels = labels.cpu().detach().numpy()
+                multimask = np.zeros_like(masks[0])
+
+                for indice, mask in zip(labels, masks):
+                    multimask[mask==1] = indice
+                ax.imshow(image.permute(1,2,0).cpu().detach().numpy())
+                ax.imshow(multimask,alpha=0.7*(multimask>0))
+                ax.axis('off')
+
+            plt.tight_layout()
+            trainer.logger.experiment["fig/val/input"].upload(fig)
+            plt.close()
+
+            with torch.no_grad():
+                _, x_hat = pl_module(batch)
+                original_sizes = [(img.shape[1],img.shape[2]) for img in all_images]  # example sizes
+
+                results = pl_module.processor.post_process_instance_segmentation(x_hat, target_sizes=original_sizes)
+
+            fig, axs = plt.subplots(2, n_cols)
+            axs = axs.flatten()
+
+            for ax, image, result in zip(axs, all_images, results):
+                multimask = result['segmentation'].cpu().detach().numpy()
+
+                ax.imshow(image.permute(1,2,0).cpu().detach().numpy())
+                ax.imshow(multimask,alpha=0.7*(multimask>0))
+                if len(result['segments_info']) !=0 :
+                    lenth = len(result['segments_info'])
+                    print(f'non zero segments: {lenth}')
+
+                ax.axis('off')  # Optional: Turn off axes for better visualization
+
+            plt.tight_layout()
+            trainer.logger.experiment["fig/val/output"].upload(fig)
+            plt.close()
