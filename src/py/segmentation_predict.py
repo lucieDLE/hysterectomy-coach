f2de75f6cb8f6a62a8c2abe78753e975694f5cdc
lucieDLE <dolelucie0@gmail.com>
2025-08-06 13:00:15 -0400

    enh: add faster rcnn

--- src/py/segmentation_predict.py
+++ src/py/segmentation_predict.py
@@ -1,6 +1,6 @@
 import os 
 import pdb
-os.environ['CUDA_VISIBLE_DEVICES']="1"
+os.environ['CUDA_VISIBLE_DEVICES']="0"
 import torch.multiprocessing
 torch.multiprocessing.set_sharing_strategy('file_system')
 
@@ -12,7 +12,7 @@ import torch
 from tqdm import tqdm 
 import SimpleITK as sitk
 
-from nets.segmentation import MaskRCNN
+from nets.segmentation import MaskRCNN,FasterRCNN
 from loaders.hyst_dataset import HystDataModuleSeg, TrainTransformsSeg, EvalTransformsSeg, BBXImageTestTransform
 
 def save_image(filename, np_array):
@@ -75,20 +75,53 @@ def filter_by_confidence(class_ids, scores, masks, boxes, thr):
     
   return class_ids[final_keep], scores[final_keep], masks[final_keep], boxes[final_keep]
 
-def remove_labels(df, args):
-    concats = ['Bipolar', 'Vessel Sealer', 'Robot Grasper Heat', 'Laparoscopic Scissors', 'Laparoscopic Suction', 'Robot Scissors', 'monopolarhook' ]
+laparo = [
+       'Hyst_JS_1.30.23.mp4', 
+       'Hyst_SurgU_3.21.23d_3069_3157.mp4.mp4',
+       'Cuff_Closure/Hyst_MedT_3.20.23r_2965_3187.mp4',
+       'Hyst_SurgU_3.21.23b.mp4',
+       '!Hyst_MedT_3.21.23d_1219_1482.mp4.mp4',
+       'Cuff_Closure/!Hyst_MedT_3.20.23w_1604_1612.mp4'
+]
 
-    if concats is not None:
-        replacement_val =20 # df.loc[ df['Instrument Name'] == concats[0]]['class'].unique()
-        df.loc[ df['Instrument Name'].isin(concats), "class" ] = replacement_val
+robo = ['Hyst_BB_4.14.23.mp4', 
+        'Hyst_BB_4.17.23_3630_3847.mp4.mp4',
+       'Cuff_Closure/Hyst_DPM_12.14.22e_3462_4190.mp4',
+       'Cuff_Closure/Hyst_BB_4.17.23_3936_4064.mp4',
+       'Hyst_BB_1.20.23b.mp4',
+       'Cuff_Closure/Hyst_BB_1.20.23a_3525_3753.mp4',
+       'Hyst_LHC_4.4.2023c.mp4' ]
 
-    unique_classes = sorted(df[args.class_column].unique())
+# concats = ['Bipolar', 'Vessel Sealer', 'Robot Grasper Heat', 'Laparoscopic Scissors', 'Laparoscopic Suction', 'Robot Scissors', 'monopolarhook' ]
+concats = ['Bipolar', 'Vessel Sealer', 'Scissors', 'Suction', 'Robot Scissors', 'monopolarhook' ]
+
+
+
+def construct_class_mapping(df_labels, args):
+
+    # df_labels = df_labels.loc[df_labels['Video Name'].isin(robo)]
+    df_labels = df_labels.loc[df_labels[args.label_column] != 'Needle']
+
+    df_labels.loc[ df_labels[args.label_column].isin(concats), args.label_column ] = 'Others'
+
+    unique_classes = sorted(df_labels[args.label_column].unique())
     class_mapping = {value: idx+1 for idx, value in enumerate(unique_classes)}
+    return class_mapping
 
-    df[args.class_column] = df[args.class_column].map(class_mapping)
-    df.rename(columns={concats[0]: 'Others'})
+def remove_labels(df, class_mapping, args):
 
-    print(f"{df[[args.class_column]].drop_duplicates()}")
+    # concats = ['Bipolar', 'Vessel Sealer', 'Robot Grasper Heat', 'Laparoscopic Scissors', 
+    #            'Laparoscopic Suction', 'Robot Scissors', 'monopolarhook' ]
+    # concats = ['Bipolar', 'Vessel Sealer', 'Grasper Heat', 'Scissors', 
+    #            'Suction', 'monopolarhook' ]
+    # df = df.loc[df['Video Name'].isin(robo)]
+    df = df.loc[df[args.label_column] != 'Needle']
+
+    df.loc[ df[args.label_column].isin(concats), args.label_column ] = 'Others'
+
+    df[args.class_column] = df[args.label_column].map(class_mapping)
+
+    print(f"{df[[args.label_column, args.class_column]].drop_duplicates()}")
     return df.reset_index()
 
 
@@ -99,13 +132,16 @@ def main(args):
   df_test = pd.read_csv(args.csv_test)
 
 
-  df_train = remove_labels(df_train, args)
-  df_val = remove_labels(df_val, args)
-  df_test = remove_labels(df_test, args)
+  df_labels = pd.concat([df_train, df_val, df_test])
+  class_mapping = construct_class_mapping(df_labels, args)
 
-  print(df_train[ ['Instrument Name', 'class']].value_counts())
-  print(df_val[ ['Instrument Name', 'class']].value_counts())
-  print(df_test[ ['Instrument Name', 'class']].value_counts())
+  df_train = remove_labels(df_train, class_mapping,args)
+  df_val = remove_labels(df_val, class_mapping,args)
+  df_test = remove_labels(df_test, class_mapping,args)
+
+  print(df_train[ [args.label_column, args.class_column]].value_counts())
+  print(df_val[ [args.label_column, args.class_column]].value_counts())
+  print(df_test[ [args.label_column, args.class_column]].value_counts())
 
   num_classes = len(df_train[args.class_column].unique()) + 1 # background
   args_params = vars(args)
@@ -122,7 +158,12 @@ def main(args):
   test_dl = ttdata.test_dataloader()
   test_ds = ttdata.test_ds
 
-  model = MaskRCNN.load_from_checkpoint(args.model)
+  if args.nn == 'FasterRCNN':
+      model = FasterRCNN.load_from_checkpoint(args.model, **vars(args))
+  else:
+      model = MaskRCNN.load_from_checkpoint(args.model, **vars(args)) 
+
+  # model = MaskRCNN.load_from_checkpoint(args.model)
   model.eval()
   model.cuda()
 
@@ -145,44 +186,43 @@ def main(args):
       outputs = model.forward(img.cuda(), mode='test')
 
       pred_boxes = outputs[0]['boxes'].cpu().detach().numpy()
-      pred_masks = outputs[0]['masks'].cpu().detach().numpy()
+      # pred_masks = outputs[0]['masks'].cpu().detach().numpy()
       pred_labels = outputs[0]['labels'].cpu().detach().numpy()
       pred_scores = outputs[0]['scores'].cpu().detach().numpy()
 
-      gt_masks = batch[0]['masks']
-      gt_boxes = ttdata.compute_bb_mask(gt_masks, pad=0.01).numpy()
+      # gt_masks = batch[0]['masks']
+      # gt_boxes = ttdata.compute_bb_mask(gt_masks, pad=0.01).numpy()
+      gt_boxes = batch[0]['boxes'].cpu().detach().numpy()
       gt_labels = batch[0]['labels'].cpu().detach().numpy()
 
-      gt_mul_mask = construct_multi_mask(gt_masks, gt_labels)
-      frame_name = test_ds.data.df_subject.iloc[idx][args.img_column].split('img/')[1].split('.nrrd')[0] 
-      gt_filename = os.path.join(outdir_masks, frame_name + '_mask_gt.nrrd')
+      # gt_mul_mask = construct_multi_mask(gt_masks, gt_labels)
+      # frame_name = test_ds.data.df_subject.iloc[idx][args.img_column].split('img/')[1].split('.nrrd')[0] 
+      # gt_filename = os.path.join(outdir_masks, frame_name + '_mask_gt.nrrd')
       # save_image(gt_filename, gt_mul_mask)
 
       # add an else: we should save mask with nothing to compare with ground truth with no detection 
       # -> not really fair to do it like this 
-      if (pred_scores >=args.thr_score).any():
+      # if (pred_scores >=args.thr_score).any():
 
-        refined_labels, refined_scores, refined_masks, refined_boxes = filter_by_confidence(pred_labels, pred_scores, pred_masks, pred_boxes, thr=args.thr_score)
+      #   refined_labels, refined_scores, refined_masks, refined_boxes = filter_by_confidence(pred_labels, pred_scores, pred_masks, pred_boxes, thr=args.thr_score)
         
-        refined_masks[refined_masks>args.thr_mask] = 1
-        refined_masks = refined_masks[:,0,:,:]
+      #   refined_masks[refined_masks>args.thr_mask] = 1
+      #   refined_masks = refined_masks[:,0,:,:]
 
-        # 2. Build multi - masks
-        pred_mul_mask = construct_multi_mask(refined_masks, refined_labels)
+      #   # 2. Build multi - masks
+      #   pred_mul_mask = construct_multi_mask(refined_masks, refined_labels)
 
 
-        pred_filename = os.path.join(outdir_masks, frame_name + '_mask_pred.nrrd')
+      # pred_filename = os.path.join(outdir_masks, frame_name + '_mask_pred.nrrd')
         # save_image(pred_filename, pred_mul_mask)
 
         # 1. save bounding boxes, scores and labels in pickle
-        data_out.append({'gt_boxes': gt_boxes,
-                        'gt_labels': gt_labels,
-                        'gt_mask_path': gt_filename,
-                        'pred_boxes': refined_boxes,
-                        'pred_labels': refined_labels,
-                        'pred_scores': refined_scores,
-                        'pred_mask_path': pred_filename,
-                        })
+      data_out.append({'gt_boxes': gt_boxes,
+                      'gt_labels': gt_labels,
+                      'pred_boxes': pred_boxes,
+                      'pred_labels': pred_labels,
+                      'pred_scores': pred_scores,
+                      })
 
 
   # 4. save labels and scores for analysis
@@ -206,12 +246,13 @@ if __name__ == '__main__':
   input_group.add_argument('--img_column', help='Image/video column name', type=str, default='img_path')
   input_group.add_argument('--seg_column', help='segmentation column name', type=str, default='seg_path')
   input_group.add_argument('--class_column', help='Class column', type=str, default='class')
+  input_group.add_argument('--label_column', help='label column', type=str, default='label')
   input_group.add_argument('--mount_point', help='Dataset mount directory', type=str, default="./")
   input_group.add_argument('--num_workers', help='Number of workers for loading', type=int, default=4)
-  
+  input_group.add_argument('--nn', help='neural network type', type=str, default='MaskRCNN')
+
   input_group.add_argument('--thr_score', help='confidence score threshold to select best predictions', type=float, default=0.2)
   input_group.add_argument('--thr_mask', help='mask threshold to cleanup mask', type=float, default=0.1)
-  output_group = parser.add_argument_group('Output')
   
   args = parser.parse_args()
   main(args)
